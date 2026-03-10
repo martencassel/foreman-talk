fe9c64d2e01b7a860f721ba727024f0cd8cab3b5
Mårten Cassel <marten.cassel@gmail.com>
2026-03-10 17:01:11 +0100

    more

---
+++ powershell/create-vm.ps1
@@ -0,0 +1,23 @@
+New-VM -Name "rhel2" -Generation 2 -MemoryStartupBytes 8GB `
+       -NewVHDPath "C:\Users\U01MCA\vms\rhel2.vhdx" `
+       -NewVHDSizeBytes 40GB -SwitchName "Default Switch"
+
+# Add second NIC
+Add-VMNetworkAdapter -VMName "rhel2" -SwitchName "External" -Name "ExternalNIC"
+
+# Disable Secure Boot + attach ISO
+Set-VMFirmware -VMName "rhel2" -EnableSecureBoot Off
+Add-VMDvdDrive -VMName "rhel2"
+Set-VMDvdDrive -VMName "rhel2" -Path "C:\Users\U01MCA\Downloads\rhel-9.7-x86_64-dvd.iso"
+
+# Set boot order: DVD → HDD → NIC
+$dvd = Get-VMDvdDrive -VMName "rhel2"
+$hdd = Get-VMHardDiskDrive -VMName "rhel2"
+$nic = Get-VMNetworkAdapter -VMName "rhel2" | Select-Object -Last 1   # NIC last
+
+Set-VMFirmware -VMName "rhel2" -BootOrder $dvd,$hdd,$nic
+
+# Start the VM
+Start-VM -VMName "rhel2"
+
+# Delete Remove-VM -VMName rhel2 del C:\users\U01MCA\vms\rhel2.vhdx
